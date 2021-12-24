edb3dec0e2dc25ca5d89eeb4873da00c0662635d
rubenphi <rubenphii@gmail.com>
2021-12-24 10:50:36 -0500

    ordenando migraciones 7

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
     $this->call(GrupoSeeder::class);
     $this->call(UserSeeder::class);
     $this->call(CuestionarioSeeder::class);
-    $this->call(OpcionSeeder::class);
     $this->call(PreguntaSeeder::class);
+    $this->call(OpcionSeeder::class);
     $this->call(RespuestaSeeder::class);
 
   }
