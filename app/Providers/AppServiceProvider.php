0fdbcfd6ae97cfa0d5bb0b147310795b35584a6a
rubenphi <rubenphii@gmail.com>
2021-12-24 10:06:45 -0500

    ordenando migraciones 1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,34 +11,20 @@ class AppServiceProvider extends ServiceProvider
   * Register any application services.
   *
   * @return void
-  
+  */
   public function register() {
     //
   }
-  
-  */
-  
-public function register()
-{
-   if (env('REDIRECT_HTTPS')) {
-       $this->app['request']->server->set('HTTPS', true);
-   }
-}
+
 
   /**
   * Bootstrap any application services.
   *
   * @return void
-
+*/
   public function boot()
   {
   Schema::defaultStringLength(191);
-  }
-  */
-  public function boot(UrlGenerator $url) {
-    if (env('REDIRECT_HTTPS')) {
-      $url->formatScheme('https://');
-    }
   }
 
 }
