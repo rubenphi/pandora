2c363cfca353a4137abcbc55758c97155e85eb9a
rubenphi <rubenphii@gmail.com>
2021-12-23 20:57:43 -0500

    Backend finalizado con autenticacion y preparado para heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,26 +4,41 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Routing\UrlGenerator;
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+  /**
+  * Register any application services.
+  *
+  * @return void
+  
+  public function register() {
+    //
+  }
+  
+  */
+  
+public function register()
+{
+   if (env('REDIRECT_HTTPS')) {
+       $this->app['request']->server->set('HTTPS', true);
+   }
+}
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        Schema::defaultStringLength(191);
+  /**
+  * Bootstrap any application services.
+  *
+  * @return void
+
+  public function boot()
+  {
+  Schema::defaultStringLength(191);
+  }
+  */
+  public function boot(UrlGenerator $url) {
+    if (env('REDIRECT_HTTPS')) {
+      $url->formatScheme('https://');
     }
+  }
+
 }
