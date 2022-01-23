65908a16621fb9e158d7e0ece8dc9c0fab2d8bbe
rubenphi <rubenphii@gmail.com>
2022-01-23 17:40:44 -0500

    se corrige faltante

--- database/migrations/2022_01_23_213345_existe_faltante.php
+++ database/migrations/2022_01_23_213345_existe_faltante.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Existe extends Migration
+class Existe_faltante extends Migration
 {
     /**
      * Run the migrations.
