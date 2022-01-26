81870e5e33097610f683ed2d1d9a7646b81dbbd1
rubenphi <rubenphii@gmail.com>
2022-01-26 18:09:31 -0500

    Update respaldo.sql

--- backup/respaldo.sql
+++ backup/respaldo.sql
@@ -3,6 +3,7 @@
 -- Host: localhost    Database: pandora
 -- ------------------------------------------------------
 -- Server version	8.0.27-0ubuntu0.21.04.1
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -198,15 +199,6 @@ UNLOCK TABLES;
 -- Table structure for table `migrations`
 --
 
-    
-        
-          
-    
-
-        
-    
-    @@ -207,7 +27,7 @@ CREATE TABLE `migrations` (
-  
 DROP TABLE IF EXISTS `migrations`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -219,16 +211,6 @@ CREATE TABLE `migrations` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
-
-    
-        
-          
-    
-
-        
-    
-    @@ -216,328 +36,8 @@ CREATE TABLE `migrations` (
-  
 -- Dumping data for table `migrations`
 --
 
@@ -559,19 +541,11 @@ UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-
-    
-        
-          
-    
-
-        
-    
-    @@ -548,4 +48,4 @@ UNLOCK TABLES;
-  
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-01-25 10:49:29
